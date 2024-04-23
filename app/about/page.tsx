ec72c3186ad557cb13c3af9cbd84fec674afbcd3
Abdulhakim Aminu Ibrahim <lethalskillzzjr@gmail.com>
2024-04-23 21:16:26 +0100

    carousel, working progress

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,19 @@
+import AboutUs from "@/components/about/aboutUs";
+import Mission from "@/components/about/mission";
 import AchievementsSection from "@/components/home/achievements";
 import FaqSection from "@/components/home/faq";
 import HeroSection from "@/components/home/hero";
 import PartnersSection from "@/components/home/partners";
 import PortfolioSection from "@/components/home/portfolio";
+import ServicesSection from "@/components/home/services";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-        <div>About page</div>
+        <AboutUs />
+        <Mission />
+        <ServicesSection />
     </main>
   );
 }
