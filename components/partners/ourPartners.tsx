d01e76b4b4f7824d89afd21e107758f88f11326f
Abdulhakim Aminu Ibrahim <lethalskillzzjr@gmail.com>
2024-05-02 10:51:15 +0100

    partners, in working progress

---
+++ components/partners/ourPartners.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const OurPartners: React.FC = () => {
+  return (
+    <div className='w-full'>
+      <div className='ourPartner lg:w-[1440px] lg:h-[753px]'>
+        
+      </div>
+      <div className=''></div>
+    </div>
+  )
+}
+
+export default OurPartners
