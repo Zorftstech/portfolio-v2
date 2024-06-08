5ce58eaf9bd75eca9bb929ca5e49a547949e2184
Abdulhakim Aminu Ibrahim <lethalskillzzjr@gmail.com>
2024-06-08 21:05:46 +0100

    whyChooseUse.tsx component added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import PartnersSection from "@/components/home/partners";
 import PortfolioSection from "@/components/home/portfolio";
 import ServicesSection from "@/components/home/services";
 import TechnologiesWeUse from "@/components/home/technologies";
+import TestimonialCard from "@/components/home/testimonialCard";
+import WhyChooseUs from "@/components/home/whyChooseUs";
 
 export default function Home() {
   return (
@@ -13,8 +15,10 @@ export default function Home() {
       <PartnersSection />
       <AchievementsSection />
       <ServicesSection />
+      <WhyChooseUs />
       <PortfolioSection />
       <TechnologiesWeUse />
+      <TestimonialCard />
       <FaqSection />
     </main>
   );
