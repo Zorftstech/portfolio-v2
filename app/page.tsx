086fdfcb6b21e657d7967b57b5fa876ab5c2ebc1
PraiseImmanuel <praiseimmanuel601@gmail.com>
2024-06-09 19:28:09 +0100

    new: Complete navbar routing

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,21 @@ import PartnersSection from "@/components/home/partners";
 import PortfolioSection from "@/components/home/portfolio";
 import ServicesSection from "@/components/home/services";
 import TechnologiesWeUse from "@/components/home/technologies";
-import { TopAndBottomWrapper } from "@/components/shared/Wrappers";
+import { SideWrapper, TopAndBottomWrapper } from "@/components/shared/Wrappers";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <TopAndBottomWrapper>
-        <HeroSection />
-        <PartnersSection />
-        <AchievementsSection />
-        <ServicesSection />
-        <PortfolioSection />
-        <TechnologiesWeUse />
-        <FaqSection />
+        <SideWrapper>
+          <HeroSection />
+          <PartnersSection />
+          <AchievementsSection />
+          <ServicesSection />
+          <PortfolioSection />
+          <TechnologiesWeUse />
+          <FaqSection />
+        </SideWrapper>
       </TopAndBottomWrapper>
     </main>
   );
