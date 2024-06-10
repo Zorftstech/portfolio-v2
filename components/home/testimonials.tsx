3d055ba9af0b39d2f2e05bb76ba6b99913bf3959
Yusuf Iysah  | UX Engineer <88023259+Iysah@users.noreply.github.com>
2024-06-10 11:33:21 +0100

    corrected footer links and testimonial sections

---
+++ components/home/testimonials.tsx
@@ -0,0 +1,28 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import Marquee from "react-fast-marquee";
+import { Technologies } from "../data";
+import TestimonialCard from "./testimonialCard";
+
+const Testimonials: React.FC = () => {
+	return (
+		<div className="mx-auto py-24 mb-8 mt-40 w-full">
+			<div className="flex justify-start items-center">
+				<h4 className="text-center bg-[#F2F2F2] p-2 rounded-lg text-[#9C9AA1] w-[130px]">
+					Testimonials
+				</h4>
+			</div>
+
+			{/* ============= Section description ============= */}
+			<h3 className="text-[#3B3D3F] text-4xl lg:text-[64px] lg:leading-[82px] text-start font-bold mt-10">
+            What they said about us from the people we shared experiences with.
+			</h3>
+
+            <TestimonialCard />
+        </div>
+
+    )
+}
+
+export default Testimonials
