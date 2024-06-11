a5170f7d760e545d6ddfa626d8d2ddf3448d4d54
Yusuf Iysah  | UX Engineer <88023259+Iysah@users.noreply.github.com>
2024-06-11 20:47:04 +0100

    fixed merge conflict

--- app/faqs/page.tsx
+++ app/faqs/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Home() {
+export default function FAQ() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-        <div>Talents page</div>
+        <div>Frequently ask questions</div>
     </main>
   );
 }
