25aa1baffc2c689b180eb1357f22dfccbd1917e3
Abdulhakim Aminu Ibrahim <lethalskillzzjr@gmail.com>
2024-06-13 17:33:13 +0100

    added careers page & moved portfolioGrid to shared folder

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -1,10 +1,13 @@
-import { TopAndBottomWrapper } from "@/components/shared/Wrappers";
+import BePartOfOurTeam from "@/components/careers/bepartOfOurTeam";
+import { SideWrapper, TopAndBottomWrapper } from "@/components/shared/Wrappers";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <TopAndBottomWrapper>
-        <div>Careers page</div>
+        <SideWrapper>
+          <BePartOfOurTeam />
+        </SideWrapper>
       </TopAndBottomWrapper>
     </main>
   );
