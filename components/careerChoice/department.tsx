267b69c87a91733563c35b8efed51feff0918b9f
Yusuf Iysah  | UX Engineer <88023259+Iysah@users.noreply.github.com>
2024-06-21 14:53:12 +0100

    fixed careers page

--- components/careerChoice/department.tsx
+++ components/careerChoice/department.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-import Image from "next/image";
+import { FaArrowLeftLong } from "react-icons/fa6";
 import { careers, Careers, YourRole, YourSkills } from "../careers/data";
+import { useRouter } from 'next/navigation'
+// import router from "next/router";
 
 const Department: React.FC = () => {
   const searchParams = useSearchParams();
   const department = searchParams.get("department");
   const [career, setCareer] = useState<Careers | undefined>(careers[0]);
+  const router = useRouter()
 
   useEffect(() => {
     console.log(department);
@@ -26,9 +29,18 @@ const Department: React.FC = () => {
     <div className="mt-10 lg:mt-32">
       <div key={career?.id} className="">
         <div className="w-[372px] lg:w-[1200px] text-start">
-          <h2 className="text-[24px] lg:text-[48px] leading-[50px] lg:leading-[50px] text-[#004AAD] font-bold mb-8">
-            {career?.department}
-          </h2>
+          <div className={"flex items-center gap-3 mb-8"}>
+            <FaArrowLeftLong
+                className="text-[24px] text-center"
+                onClick={() => {
+                  router.back()
+                }}
+            />
+            <h2 className="text-[24px] lg:text-[48px] leading-[50px] lg:leading-[50px] text-[#004AAD] font-bold">
+              {career?.department}
+            </h2>
+          </div>
+
           <p className="text-[16px] lg:text-[20px] leading-[27px] lg:leading-[31px] text-[#5F5E6C]">
             {career?.description}
           </p>
