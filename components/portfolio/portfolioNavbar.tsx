fd24880a87af0b191e4dc159e11a1cfbc5ba381c
PraiseImmanuel <praiseimmanuel601@gmail.com>
2024-07-12 17:22:49 +0100

    new: Portfolio Page - Loading State Implementation

--- components/portfolio/portfolioNavbar.tsx
+++ components/portfolio/portfolioNavbar.tsx
@@ -4,12 +4,12 @@ import { useAppContext } from "@/lib/context";
 import PortfolioGrid from "../shared/portfolioGrid";
 import Image from "next/image";
 import { IProjectData } from "@/lib/types";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const projectTypes = ["All", "Design", "Development", "Deployment", "Others"];
 
-
 const PortfolioNavbar: React.FC = () => {
- const { store } = useAppContext();
+  const { store } = useAppContext();
   const { projects } = store;
   const [activeButton, setActiveButton] = useState<number>(0);
   const [currentProjects, setCurrentProjects] = useState<IProjectData[]>([]);
@@ -36,12 +36,12 @@ const PortfolioNavbar: React.FC = () => {
   return (
     <div className="">
       <div className="my-8 lg:my-24 overflow-x-auto w-full">
-        <div className="flex items-center md:justify-center w-[1012px] md:w-full">
+        <div className="flex items-center md:justify-center w-[710px] md:w-full">
           {projectTypes.map((type, typeIndex) => (
             <div key={typeIndex} className="flex">
               <button
                 onClick={() => handleButtonClick(type, typeIndex)}
-                className={`text-[16px] leading-[24px] border-[1px] rounded-full px-5 lg:px-5 py-5 mx-3 min-w-[120px] ${
+                className={`text-sm lg:text-base leading-[24px] border-[1px] rounded-full px-4 lg:px-5 py-4 lg:py-5 mx-3 min-w-[130px] ${
                   activeButton === typeIndex
                     ? "bg-[#004AAD] text-white border-blue-500"
                     : "bg-[] text-[#9897A7] border-[#3B3D3F]"
@@ -51,11 +51,34 @@ const PortfolioNavbar: React.FC = () => {
                 {type}
               </button>
             </div>
-        ))}
+          ))}
         </div>
       </div>
+      <div className="mx-auto my-8 lg:my-24">
+        {/* =========== portfolio grid ============= */}
+        {projects?.length !== 0 ? (
+          <PortfolioGrid projects={currentProjects} />
+        ) : (
+          <ProjectsLoadingState />
+        )}
+        <button className="mx-auto flex items-center justify-between w-[152px] h-[48px] bg-[#454646] rounded-full px-6 mt-20">
+          <Image src={"Vector-4.svg"} alt="" width={15.5} height={4} />
+          <span className="text-white">Load More</span>
+        </button>
+      </div>
     </div>
-    );
+  );
 };
 
 export default PortfolioNavbar;
+
+export const ProjectsLoadingState = () => {
+  return (
+    <SkeletonTheme baseColor="#313134" highlightColor="#666">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 my-6">
+        <Skeleton className="h-[300px]" />
+        <Skeleton className="h-[300px]" />
+      </div>
+    </SkeletonTheme>
+  );
+};
